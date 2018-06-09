6ccab3f301bb036687bfabe9e6b8628b7e98a0b5
Alexia <alexiaharalambous@gmail.com>
2018-06-09 12:29:18 +0100

    Added fields on database, need to fix forms and their submitions

--- discountCard/forms.py
+++ discountCard/forms.py
@@ -7,10 +7,14 @@ from django.contrib.auth.models import User
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
+    phone = forms.CharField()
+    address = models.CharField()
+    city = models.CharField()
+    country = models.CharField()
 
     class Meta:
             model = User
-            fields = ('username','first_name', 'last_name', 'email','password')
+            fields = ('username','password','first_name', 'last_name', 'email','phone','address','city','country')
 
 class LoginForm(forms.Form):
     username= forms.CharField(label='Username', max_length=30)
--- discountCard/models.py
+++ discountCard/models.py
@@ -2,21 +2,53 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Card(models.Model):
-    card_number = models.AutoField(primary_key=True)
-    date_joined = models.DateField(null=False)
-    renewal_date = models.DateField(null=False)
 
 class Profile(models.Model):
     type_CHOICES = {
         ("Client","Client"),
         ("Partner", "Partner"),
     }
+    city_CHOICES = {
+        ("Limassol","Limassol"),
+        ("Nicosia", "Nicosia"),
+        ("Paphos", "Paphos"),
+        ("Paralimni", "Paralimni"),
+        ("Larnaka", "Larnaka"),
+    }
+    country_CHOICES = {
+        ("Cyprus","Cyprus"),
+        ("Greece", "Greece"),
+    }
     id = models.AutoField(primary_key=True)
     user=models.OneToOneField(User, on_delete=models.PROTECT)
     account_type = models.CharField(choices = type_CHOICES,default = "Client", max_length=10)
-    card_number = models.OneToOneField(Card, on_delete=models.CASCADE,)
+    phone = models.CharField(max_length=12,default="")
+    address = models.CharField(max_length = 40,default="")
+    city = models.CharField(choices = city_CHOICES, max_length=20,default="")
+    country = models.CharField(choices = country_CHOICES, max_length=10,default="")
+
+
 
     # Override the __unicode__() method to return out something meaningful!
-    def __unicode__(self):
-        return self.id
+    def __str__(self):
+        return self.user.first_name
+
+
+
+class Card(models.Model):
+    type_CHOICES= {
+        ("Synolic Personal","Synolic Personal"),
+        ("Synolic Corporate", "Synolic Corporate"),
+        ("Synolic Visitors", "Synolic Visitors"),
+        ("Synolic Youth", "Synolic Youth"),
+
+    }
+    card_number = models.AutoField(primary_key=True)
+    date_joined = models.DateField(null=False)
+    renewal_date = models.DateField(null=False)
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE,default="")
+    card_type = models.CharField(choices = type_CHOICES,max_length=30,default="")
+
+    # Override the __unicode__() method to return out something meaningful!
+    def __str__(self):
+        return str(self.card_number)
